c631243d45222d5acaf8e1caf737b34fff4a1d65
Ethan Sternke <ethan.sternke@gmail.com>
2018-04-23 10:01:52 -0400

    added error handling and removed hardcoded IPs

--- views/home.js
+++ views/home.js
@@ -1,4 +1,5 @@
 let latestFetchStamp = Date.now();
+let errorCount = 0;
 // console.log(latestFetchStamp);
 
 function fetchImages() {
@@ -11,14 +12,23 @@ function fetchImages() {
                 after: latestFetchStamp
             }),
         })
-        .then(response => response.json())
-        .then(data => {
+        .then(res => res.json())
+        .then(duck => {
             // console.log(data);
-            for (let i = 0; i < data.images.length; i++) {
-                document.getElementById("images").innerHTML += `<img src="${data.images[i]}">`;
+            for (let i = 0; i < duck.images.length; i++) {
+                document.getElementById("images").innerHTML += `<img src="${duck.images[i]}">`;
             }
             latestFetchStamp = Date.now();
             setTimeout(fetchImages, 5000);
+        })
+        .catch(error => {
+            errorCount++;
+            if (errorCount > 1) {
+                console.log("Another error, stopping polling.");
+            } else {
+                console.log("Error, trying again...");
+                fetchImages();
+            }
         })
 }
 
